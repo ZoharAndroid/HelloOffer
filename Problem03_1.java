9003cb932e88a2e79f796e125294168e3bc3a8b6
ZoharAndroid <1048132071@qq.com>
2019-03-01 23:08:45 +0800

    create repository

---
+++ Problem03_1.java
@@ -0,0 +1,31 @@
+package offer;
+
+/**
+ * 面试题3
+ * 题目1：找出数组中重复的数字。在一个长度为n的数组里的所有数字都在0~n-1的范围内，数组中
+ * 某些数字是重复的，但是不知道有几个数字是重复的，找出重复的数字。
+ * 要求：时间复杂度O(n),空间复杂度O(1)
+ * 
+ * @author zohar
+ *
+ */
+public class Problem03_1 {
+	
+	/**
+	 * 
+	 * @param numbers：一个整形数组 
+	 * @param length：数组的长度
+	 * @param duplication：数组中的一个重复的值
+	 * @return
+	 * 		true：输入有效，并且数组中存在重复的数字
+	 * 		false：输入数组无效，或者数组中不包含重复的数字
+	 */
+	private boolean duplicate(int[] numbers,int length,int[] duplication) {
+		
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
